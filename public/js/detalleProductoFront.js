a13e579981d1f18b4379514dc1a96516b68f884f
AyrtonRada <axel582011@hotmail.com>
2022-04-04 17:48:08 -0300

    añadida funcion de Ficha Tecnica

--- public/js/detalleProductoFront.js
+++ public/js/detalleProductoFront.js
@@ -3,16 +3,25 @@ window.addEventListener("load", () => {
     let videoHTML = document.getElementById("video-html");
     let botonDescripcion = document.getElementById("boton-descripcion");
     let botonVideo = document.getElementById("boton-video");
-
+    let botonFichaTecnica = document.getElementById('descripcion-ficha-tecnica')
+    let fichaTecnica = document.getElementById('ficha-tecnica')
+    descripcionProducto.style.display ='none'
     botonVideo.addEventListener("click", () => {
         descripcionProducto.style.display = "none";
-
+        fichaTecnica.style.display = 'none'
         videoHTML.style.display = "block";
     })
 
     botonDescripcion.addEventListener("click", () => {
-        descripcionProducto.style.display = "block";
-
+        fichaTecnica.style.display = 'none'
         videoHTML.style.display = "none";
+        descripcionProducto.style.display = "block";
     })
+
+    botonFichaTecnica.addEventListener('click', function(){
+        descripcionProducto.style.display = 'none'
+        videoHTML.style.display = 'none'
+        fichaTecnica.style.display = 'block'
+    })
+
 })
